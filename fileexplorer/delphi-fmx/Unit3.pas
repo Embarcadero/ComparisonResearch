5f297a93f1af4413ecf3f02e1e7ea073c0a248b0
Serhii M. Kheilyk <1171495+lynxnake@users.noreply.github.com>
2020-10-31 05:42:08 +0200

    Draw all GUI

--- fileexplorer/delphi-fmx/Unit3.pas
+++ fileexplorer/delphi-fmx/Unit3.pas
@@ -4,28 +4,36 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Menus;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Menus,
+  FMX.Controls.Presentation, FMX.Edit, FMX.Layouts, System.Actions,
+  FMX.ActnList, FMX.StdActns, System.Rtti, FMX.Grid.Style, FMX.ScrollBox,
+  FMX.Grid, FMX.Header, FMX.StdCtrls, FMX.TreeView, FMX.TabControl;
 
 type
   TForm3 = class(TForm)
     WinMenu: TMenuBar;
-    MacMenu: TMainMenu;
     WinMenuFile: TMenuItem;
-    MacMenuFile: TMenuItem;
     WinMenuRun: TMenuItem;
     WinMenuBrowseForFolder: TMenuItem;
-    WinMenuExit: TMenuItem;
-    MacMenuBrowseForFolder: TMenuItem;
-    MacMenuRun: TMenuItem;
-    MacMenuExit: TMenuItem;
     WinMenuEdit: TMenuItem;
     WinMenuCopy: TMenuItem;
     WinMenuWindow: TMenuItem;
     WinMenuArrange: TMenuItem;
-    MacMenuEdit: TMenuItem;
-    MacMenuCopy: TMenuItem;
-    MacMenuWindow: TMenuItem;
-    MacMenuArrange: TMenuItem;
+    ActionList1: TActionList;
+    MenuItem1: TMenuItem;
+    Layout1: TLayout;
+    Edit1: TEdit;
+    FileExit1: TFileExit;
+    Edit2: TEdit;
+    Layout2: TLayout;
+    TreeView1: TTreeView;
+    Splitter1: TSplitter;
+    Layout3: TLayout;
+    Header1: THeader;
+    Grid1: TGrid;
+    TabControl1: TTabControl;
+    TabItem1: TTabItem;
+    TabItem2: TTabItem;
   private
     { Private declarations }
   public
