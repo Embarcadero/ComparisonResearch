aaebe8958da6c0f1135a7662bc8376ebbb460424
Serhii M. Kheilyk <1171495+lynxnake@users.noreply.github.com>
2020-11-09 14:58:14 +0200

    Folders Treeview is filled and working

--- fileexplorer/delphi-fmx/FMXFileExplorer.dpr
+++ fileexplorer/delphi-fmx/FMXFileExplorer.dpr
@@ -4,13 +4,13 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   uMain in 'uMain.pas' {Form3},
-  uDM in 'uDM.pas' {DataModule2: TDataModule};
+  uDM in 'uDM.pas' {dm: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm3, Form3);
-  Application.CreateForm(TDataModule2, DataModule2);
+  Application.CreateForm(Tdm, dm);
   Application.Run;
 end.
--- fileexplorer/delphi-fmx/uDM.pas
+++ fileexplorer/delphi-fmx/uDM.pas
@@ -6,17 +6,18 @@ uses
   System.SysUtils, System.Classes, System.Actions, FMX.ActnList, FMX.StdActns;
 
 type
-  TDataModule2 = class(TDataModule)
+  Tdm = class(TDataModule)
     ActionList1: TActionList;
     FileExit1: TFileExit;
   private
-    { Private declarations }
+    function IsWindowsDrivePath(Path: string): Boolean;
   public
+    function GetFilename(Path: string): string;
     function GetSubFolders(Path: string): TArray<string>;
   end;
 
 var
-  DataModule2: TDataModule2;
+  dm: Tdm;
 
 implementation
 
@@ -29,12 +30,27 @@ uses
 
 { TDataModule2 }
 
-function TDataModule2.GetSubFolders(Path: string): TArray<string>;
+function Tdm.GetFilename(Path: string): string;
+begin
+  if (Path = PathDelim) then
+    Result := Path
+  else
+    if IsWindowsDrivePath(Path) then
+      Result := Copy(Path, 1, 2)
+    else
+      Result := TPath.GetFileName(Path)
+end;
+
+function Tdm.GetSubFolders(Path: string): TArray<string>;
 begin
   Assert(Path.Length > 0);
   if (Path.Length <> 1) or not IsPathDelimiter(Path, High(Path)) then
-  begin
-  end
+    try
+      Result := TDirectory.GetDirectories(Path);
+    except
+      on E: EDirectoryNotFoundException do
+        Result := nil
+    end
   else
   begin
     {$IFDEF MSWINDOWS}
@@ -43,6 +59,12 @@ begin
       {$MESSAGE FATAL 'TODO: implement}
     {$ENDIF}
   end;
+end;
+
+function Tdm.IsWindowsDrivePath(Path: string): Boolean;
+begin
+  Result := (TOSVersion.Platform = pfWindows) and (Path.Length = 3) and
+    Path.Contains(':\');
 end;
 
 end.
