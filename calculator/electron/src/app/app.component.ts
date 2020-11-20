de6bd207cf9d5ff4ea300d56c208d33558245086
Heru Susanto <heruxi@gmail.com>
2020-11-20 23:45:04 +0700

    update button click

--- calculator/electron/src/app/app.component.ts
+++ calculator/electron/src/app/app.component.ts
@@ -8,13 +8,63 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Calculator';
   calc_result: number = 0;
+  old_value: number = 0;
+  operator: string = '';
 
   number_click(newNumber: number) {
-    if (this.calc_result == 0 && newNumber == 0) {
-      this.calc_result = 0; 
+    if (this.operator == '') {
+      if (this.calc_result == 0 && newNumber == 0) {
+        this.calc_result = 0; 
+      }else {
+        this.calc_result = parseFloat(this.calc_result.toString() + newNumber.toString()); 
+      }
     }else {
-      this.calc_result = parseFloat(this.calc_result.toString() + newNumber.toString()); 
+      this.old_value = this.calc_result;
+      this.calc_result = newNumber;
     }
+  }
+
+  kali_click() {
+    this.operator = 'x';
+  }
+
+  tambah_click() {
+    this.operator = '+';
+  }
+
+  min_click() {
+    this.operator = '-';
+  }
+
+  samadengan_click() {
+    switch (this.operator) {
+      case '÷':
+        this.calc_result = this.old_value / this.calc_result;
+        break;
+      case 'x':
+          this.calc_result = this.old_value * this.calc_result;
+          break;
+      case '+':
+          this.calc_result = this.old_value + this.calc_result;
+          break;
+      case '-':
+          this.calc_result = this.old_value - this.calc_result;
+          break;
+      default:
+        break;
+    }
+  }
+
+  bagi_click() {
+    this.operator = '÷';
+  }
+
+  kuadrat_click() {
+    this.calc_result = Math.pow(this.calc_result, 2);
+  }
+
+  seperx_click() {
+    this.calc_result = 1 / this.calc_result;
   }
 
   squareroot_click() {
