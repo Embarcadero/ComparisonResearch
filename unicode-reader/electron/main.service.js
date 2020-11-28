a83b9ddbef7504f17a5e7dadcfd8e1dbe5280333
Heru Susanto <heruxi@gmail.com>
2020-11-28 12:05:52 +0700

    update main service

--- unicode-reader/electron/main.service.js
+++ unicode-reader/electron/main.service.js
@@ -1,18 +1,20 @@
-const { DbConnection } = require('./db.connection');
-const { ipcMain } = require('electron');
 let Parser = require('rss-parser');
 
 class MainService {
 
-    run = () => {
-        this.dbConnection = new DbConnection();
+    constructor(dbConnection, ipcMain) {
+        this.dbConnection = dbConnection;
+        this.ipcMain = ipcMain;
+    }
+
+    run(){
         ipcMain.on('qryGetChannels', (event) => {
             let result = this.dbConnection.queryAsync('select * from channels');
             event.returnValue = result;
         });
     }
 
-    readRSS = async () => {
+    async readRSS(){
         let parser = new Parser();
         let feed = await parser.parseURL('https://blogs.embarcadero.com/ja/feed/');
         console.log(feed.title);
