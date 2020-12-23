c0fc03f0093f602aabafa5a474f122325f9d9c2b
dhirajsakariya <dhirajsakariya@gmail.com>
2020-12-23 18:46:45 +0530

    push code for wpf Github explorer

---
+++ github-recent-explorer/wpf/GitHubExplorer/GitHubExplorer/Models/GithubResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubExplorer.Models
+{
+    public class GithubResult
+    {
+        public int total_count { get; set; }
+        public bool incomplete_results { get; set; }
+        public List<Item> items { get; set; }
+    }
+}
---
+++ github-recent-explorer/wpf/GitHubExplorer/GitHubExplorer/Models/License.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubExplorer.Models
+{
+    public class License
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string spdx_id { get; set; }
+        public string url { get; set; }
+        public string node_id { get; set; }
+    }
+}
