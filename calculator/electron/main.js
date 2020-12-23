129531aea77c50f0fb41e2fdcb7d02918b0cf877
ildrummer <leone.aa@gmail.com>
2020-12-23 16:36:50 -0700

    Reverr "Reorganized the Electron and WPF Calculators" Error with Electron files disappearing
    This reverts commit 4a5cc9dc8d6b211f3dae82fe1958aa9a056d920f.

---
+++ calculator/electron/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require("path");
+const url = require("url");
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 500,
+    height: 485,
+    vibrancy: 'ultra-dark',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.setOpacity(10);
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, '/dist/calculator/index.html'),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  // win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
---
+++ calculator/electron/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      routes, 
+      {onSameUrlNavigation: 'reload'}
+    )],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
