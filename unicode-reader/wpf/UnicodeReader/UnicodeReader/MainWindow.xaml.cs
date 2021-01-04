98ab99ca3695996a5b19073eab4994a5b6a26834
dhirajsakariya <dhirajsakariya@gmail.com>
2021-01-04 17:08:46 +0530

    Implemented Unicode Reader

---
+++ unicode-reader/wpf/UnicodeReader/UnicodeReader/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UnicodeReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            feedData.Navigated += new NavigatedEventHandler(feedData_Navigated);
+        }
+
+        public static readonly DependencyProperty BindableSourceProperty =
+            DependencyProperty.RegisterAttached("BindableSource", typeof(string), typeof(MainWindow), new UIPropertyMetadata(null, BindableSourcePropertyChanged));
+
+        public static string GetBindableSource(DependencyObject obj)
+        {
+            return (string)obj.GetValue(BindableSourceProperty);
+        }
+
+        public static void SetBindableSource(DependencyObject obj, string value)
+        {
+            obj.SetValue(BindableSourceProperty, value);
+        }
+
+        public static void BindableSourcePropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser browser = o as WebBrowser;
+            if (browser != null)
+            {
+                string uri = e.NewValue as string;
+                browser.Source = !String.IsNullOrEmpty(uri) ? new Uri(uri) : null;
+            }
+        }
+
+        private void feedData_Navigated(object sender, NavigationEventArgs e)
+        {
+            SetSilent(feedData, true);
+        }
+        public static void SetSilent(WebBrowser browser, bool silent)
+        {
+            if (browser == null)
+                throw new ArgumentNullException("browser");
+
+            // get an IWebBrowser2 from the document
+            IOleServiceProvider serviceProvider = browser.Document as IOleServiceProvider;
+            if (serviceProvider != null)
+            {
+                Guid IID_IWebBrowserApp = new Guid("0002DF05-0000-0000-C000-000000000046");
+                Guid IID_IWebBrowser2 = new Guid("D30C1661-CDAF-11d0-8A3E-00C04FC9E26E");
+
+                object webBrowser;
+                serviceProvider.QueryService(ref IID_IWebBrowserApp, ref IID_IWebBrowser2, out webBrowser);
+                if (webBrowser != null)
+                {
+                    webBrowser.GetType().InvokeMember("Silent", BindingFlags.Instance | BindingFlags.Public | BindingFlags.PutDispProperty, null, webBrowser, new object[] { silent });
+                }
+            }
+        }
+
+        [ComImport, Guid("6D5140C1-7436-11CE-8034-00AA006009FA"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IOleServiceProvider
+        {
+            [PreserveSig]
+            int QueryService([In] ref Guid guidService, [In] ref Guid riid, [MarshalAs(UnmanagedType.IDispatch)] out object ppvObject);
+        }
+
+    }
+}
